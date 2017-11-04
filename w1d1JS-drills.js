7eb02d4d595d6df30f9067a8c80b0d05d6a3125d
Kayla Webb <kaylarwebb@gmail.com>
2017-11-04 15:06:36 -0400

    Moved yearofBirth into a functio

--- w1d1JS-drills.js
+++ w1d1JS-drills.js
@@ -2,13 +2,18 @@
 
 console.log('Hi, my name is Chris and I\'m 29 years old.');
 
-function whoAmI(name, age) {
-  const yearOfBirth = 2017-age;
-  console.log(`Hi, my name is ${name} and I'm ${age} years old.`);
-  console.log(`I was born in ${yearOfBirth}`);
+function yearOfBirth(age) {
+  return 2017-age;
 }
 
+function whoAmI(name, age) {
+  const yob = yearOfBirth(age);
+  console.log(`Hi, my name is ${name} and I'm ${age} years old.`);
+  console.log(`I was born in ${yob}`);
+}
+
+console.log(yearOfBirth(30));
 whoAmI('Kayla', 26);
 
-//Create a variable named yearOfBirth and calculate the year based on age (ex 2016 - age) 
-//Then add another console.log statement that outputs I was born in [insert yearOfBirth]
+//Move the year of birth calculation into a new function.
+
