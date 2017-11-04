4d0f74d8881209eae850a9d8e33430df20a0cc6a
Kayla Webb <kaylarwebb@gmail.com>
2017-11-04 14:50:29 -0400

    Change the hard coded string to one with two params

--- w1d1JS-drills.js
+++ w1d1JS-drills.js
@@ -4,8 +4,16 @@ console.log('Hi, my name is Chris and I\'m 29 years old.');
 
 //Wrap the statement in a function called whoAmI and call that function
 
-function whoAmI() {
-  console.log('Hi, my name is Chris and I\'m 29 years old.');
+// function whoAmI() {
+//   console.log('Hi, my name is Chris and I\'m 29 years old.');
+// }
+
+// whoAmI();
+
+//Change the hard coded string into a parameters, name and age
+
+function whoAmI(name, age) {
+  console.log(`Hi, my name is ${name} and I'm ${age} years old.`);
 }
 
-whoAmI();
+whoAmI('Kayla', 26);
