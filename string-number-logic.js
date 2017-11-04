15ca32538845af85cc6c692db63c39c32ddeaafd
Kayla Webb <kaylarwebb@gmail.com>
2017-11-04 16:54:44 -0400

    Got positive finite numbers to work

--- string-number-logic.js
+++ string-number-logic.js
@@ -1,8 +1,36 @@
-'use strict'
+'use strict';
 //JediName 
 
 function jediName(firstName, lastName) {
   return lastName.slice(0,3)+firstName.slice(0,2);  
 }
 
-console.log(jediName('Beyonce', 'Knowles'));
+// console.log(jediName('Beyonce', 'Knowles'));
+
+//toInfinity
+
+function beyond(num) {
+//   const infinPos = Infinity;
+//   const infinNeg = -Infinity;
+//   const finitePos = Number.isFinite(num);
+//   const finiteNeg = Number.isFinite(-num);
+//   const zero = 0;
+
+  switch(true) {
+  case Infinity:
+  case -Infinity:
+    console.log('And beyond');
+    break;
+  case Number.isFinite(num):
+    console.log('To infinity');
+    break;
+  case Number.isFinite(-num):
+    console.log('To negative infinity');
+    break;
+  case 0:
+    console.log('Staying home');
+    break;    
+  }
+}
+
+beyond(-500);
