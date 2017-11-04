90464533cacb2a943bb61333133516f38b43049d
Kayla Webb <kaylarwebb@gmail.com>
2017-11-04 16:04:24 -0400

    Try catch block is working

--- w1d1JS-drills.js
+++ w1d1JS-drills.js
@@ -1,6 +1,6 @@
 'use strict';
 
-console.log('Hi, my name is Chris and I\'m 29 years old.');
+// console.log('Hi, my name is Chris and I\'m 29 years old.');
 
 function yearOfBirth(age) {
   return 2017-age;
@@ -9,19 +9,29 @@ function yearOfBirth(age) {
 function whoAmI(name, age) {
   const yob = yearOfBirth(age);
   try {
-    yob;
-    if (yob < 0 || yob > 125) {
-      throw Error( 'invalid age');
+    let negativeAge = age < 0;
+    let olderAge = age > 125;
+    if (negativeAge) {
+      throw new Error('Age cannot be negative');
+    }
+    else if (olderAge) {
+      throw new Error('Age is over 125');
     }
   }
-  catch (Error){
-    return 'Age cannot be negative or greater than 125';
+  catch (e){
+    //using return will stop the function here; but console log with return the string//
+    return `User error: ${e.message}`;
   }
-  console.log(`Hi, my name is ${name} and I'm ${age} years old.`);
-  console.log(`I was born in ${yob}`);
+  return `Hi, my name is ${name} and I'm ${age} years old. I was born in ${yob}.`;  
 }
 
-console.log(yearOfBirth(30));
-whoAmI('Kayla', -2);
+//function runs and an error message prints to the console.//
+const berthaOver100 = whoAmI('Bertha', 126);
+console.log(berthaOver100);
+
+//assigning valid function to a variable and logging the output//
+const kaylaW = whoAmI('Kayla', 26);
+
+console.log(kaylaW);
 
 
