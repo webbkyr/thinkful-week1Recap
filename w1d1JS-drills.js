d5a844e717dfc67dc7162e6afaf87a7e016478da
Kayla Webb <kaylarwebb@gmail.com>
2017-11-04 14:47:33 -0400

    wrap the statement in a function

--- w1d1JS-drills.js
+++ w1d1JS-drills.js
@@ -1,3 +1,11 @@
 'use strict';
 
 console.log('Hi, my name is Chris and I\'m 29 years old.');
+
+//Wrap the statement in a function called whoAmI and call that function
+
+function whoAmI() {
+  console.log('Hi, my name is Chris and I\'m 29 years old.');
+}
+
+whoAmI();
