dc998f0f21b50ae95c053b0280dba4815ca7ca7f
Kayla Webb <kaylarwebb@gmail.com>
2017-11-04 23:32:40 -0400

    Function that will show days in the month

--- string-number-logic.js
+++ string-number-logic.js
@@ -54,7 +54,7 @@ function decode(message){
       result += strArray[i].charAt([2]);
     }
     else if (strArray[i][0] === 'c') {
-      result += strArray[i].charAt([3]);
+      result += strArray[i][3];
     }
     else if (strArray[i][0] === 'd') {
       result += strArray[i][4]; 
@@ -66,7 +66,50 @@ function decode(message){
   return result;
 }
 
+// console.log(decode(message));
 
-console.log(decode(message));
+//drill 4: days in a month
 
+function daysInMonth(month) {
+  switch(month) {
+  case 'January':
+    console.log('January has 31 days');
+    break;
+  case 'February':
+    console.log('February has 28 days');
+    break;
+  case 'March':
+    console.log('March has 31 days');
+    break;
+  case 'April':
+    console.log('April has 30 days');
+    break;
+  case 'May':
+    console.log('May has 31 days');
+    break;
+  case 'June':
+    console.log('June has 30 days');
+    break;
+  case 'July':
+    console.log('July has 31 days');
+    break;
+  case 'August':
+    console.log('August has 31 days');
+    break;
+  case 'September':
+    console.log('September has 30 days');
+    break;
+  case 'October':
+    console.log('October has 31 days');
+    break;
+  case 'November':
+    console.log('November has 30 days');
+    break;
+  case 'December':
+    console.log('December has 31 days');
+    break;
 
+  }
+}
+
+daysInMonth('February');
