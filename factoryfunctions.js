a35b0c3354d3a3613861ead0327606921fb937b6
Kayla Webb <kaylarwebb@gmail.com>
2017-11-05 17:35:06 -0500

    characters array

--- factoryfunctions.js
+++ factoryfunctions.js
@@ -15,7 +15,14 @@ function createCharacter (name, nickname, race, origin, attack, defense) {
         return `Your opponent takes ${x} damage and you recieve ${y} damage.`;
       }
     } 
-
   };
-
 }
+
+const characters = [
+  createCharacter('Gandalf the White', 'gandalf', 'Wizard', 'Middle Earth', 10, 6), 
+  createCharacter('Biblo Baggins', 'biblo', 'Hobbit', 'The Shire', 2, 1), 
+  createCharacter('Frodo Baggins', 'frodo', 'Hobbit', 'The Shire', 3, 2),
+  createCharacter('Aragorn son of Arathorn', 'aragorn', 'Man', 'Dunnedain', 6, 8),
+  createCharacter('Legolas', 'legoolas', 'Elf', 'Woodland Realm', 8, 5)
+];
+
